072634ada4b76314d3f0ecdbeb3e31a85b6390c0
Doukuest <33143337+Doukuest@users.noreply.github.com>
2019-03-21 12:36:03 -0500

    Pequeños avances en la gui

--- project/core.py
+++ project/core.py
@@ -7,12 +7,14 @@ class CoreRegressors:
             self.DT_R = pickle.load(input)
             self.RF_R = pickle.load(input)
             self.GB_R = pickle.load(input)
+            self.BG_R = pickle.load(input)
     def DTR_predict(self,genres_vector):
         return self.DT_R.predict(genres_vector)
     def RFR_predict(self,genres_vector):
         return self.RF_R.predict(genres_vector)
     def GBR_predict(self,genres_vector):
         return self.GB_R.predict(genres_vector)
+    def BGR_predict(self,genres_vector):
+        return self.BG_R.predict(genres_vector)
     def prueba(self):
         print self.DT_R
-        
--- project/gui.py
+++ project/gui.py
@@ -1,12 +1,35 @@
 from Tkinter import *
 from core import *
 
-#Carga de los regresores
-core = CoreRegressors()
+class Aplicacion(Frame):
+    def __init__(self, master=None):
+        master.geometry('500x500')
+        Frame.__init__(self,master)
+        self.pack()
+        self.hacerWidgets()
+        #Carga de los regresores
+        self.core = CoreRegressors()
+
+    def hacerWidgets(self):
+        #Empezar con el row en 10 para poder poner cosas antes y despues
+        self.txt_resultados = Label(self,text="Resultados").grid(columnspan=60,row=10)
+
+        self.txt_popularidad = Label(self,text="Popularidad").grid(column=10,row=15)
+        self.tbx_popularidad = Entry(self).grid(column=10,row=20)
+
+        self.txt_puntaje = Label(self,text="Puntaje").grid(column=20,row=15)
+        self.tbx_puntaje = Entry(self).grid(column=20,row=20)
+
+        self.txt_favoritos = Label(self,text="Favoritos").grid(column=30,row=15)
+        self.tbx_favoritos = Entry(self).grid(column=30,row=20)
+
+        self.txt_generos= Label(self,text="Generos").grid(column=30,row=30, sticky=E)
+
+        self.txt_config= Label(self,text="Parametros").grid(column=30, columnspan=15,row=30)
+
+
 
 #Carga de la ventana
 window = Tk()
-window.title("Discretas III")
-
-
-window.mainloop()
+app = Aplicacion(master=window)
+app.mainloop()
